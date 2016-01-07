a777e4e269158010285abaa43d8073b34d8decbe
Edward Suczewski <ed.suczewski@gmail.com>
2016-01-07 16:30:12 -0700

    Added Geofire Framework & Bridging Header
    Signed-off-by: Edward Suczewski <ed.suczewski@gmail.com>

---
+++ PubFeed/BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridgingHeader.h
+//  PubFeed
+//
+//  Created by Edward Suczewski on 1/7/16.
+//  Copyright © 2016 Mike Gilroy. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import <GeoFire/GeoFire.h>
+
+#endif /* BridgingHeader_h */
