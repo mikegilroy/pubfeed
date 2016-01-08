8c6ad2412d095b45a72e7975d9ba5259da1a8e3f
Edward Suczewski <ed.suczewski@gmail.com>
2016-01-08 13:05:24 -0700

    Revert "Create outlets for NewPostTableVC"
    This reverts commit 30ce098e939d9e1e74fb079bcfe20543a5aa537a.

--- PubFeed/NewPostTableTableViewController.swift
+++ PubFeed/NewPostTableTableViewController.swift
@@ -15,24 +15,6 @@ class NewPostTableTableViewController: UITableViewController {
     
     // MARK: Outlets
     
-    @IBOutlet weak var barNameLabel: UILabel!
-    @IBOutlet weak var emoji1: UIButton!
-    @IBOutlet weak var emoji2: UIButton!
-    @IBOutlet weak var emoji3: UIButton!
-    @IBOutlet weak var emoji4: UIButton!
-    @IBOutlet weak var emoji5: UIButton!
-    @IBOutlet weak var emoji6: UIButton!
-    @IBOutlet weak var emoji7: UIButton!
-    @IBOutlet weak var emoji8: UIButton!
-    @IBOutlet weak var emoji9: UIButton!
-    @IBOutlet weak var emoji10: UIButton!
-    @IBOutlet weak var emoji11: UIButton!
-    @IBOutlet weak var emoji12: UIButton!
-    @IBOutlet weak var emoji13: UIButton!
-    @IBOutlet weak var emoji14: UIButton!
-    @IBOutlet weak var emoji15: UIButton!
-    @IBOutlet weak var emoji16: UIButton!
-    @IBOutlet weak var textView: UITextView!
     
     // MARK: Actions
     
@@ -42,13 +24,6 @@ class NewPostTableTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     
-    }
-    
-    override func viewWillDisappear(animated: Bool) {
-        super.viewWillDisappear(true)
-        if emoji1.selected == false {
-            print("YES!")
-        }
     }
 
     
