e4489ba2337638a6bdb7d81092d6eb5de764379e
Edward Suczewski <ed.suczewski@gmail.com>
2016-01-08 13:23:31 -0700

    Add Outlets & Actions for NewPostTableVC
    Signed-off-by: Edward Suczewski <ed.suczewski@gmail.com>

--- PubFeed/NewPostTableTableViewController.swift
+++ PubFeed/NewPostTableTableViewController.swift
@@ -15,9 +15,106 @@ class NewPostTableTableViewController: UITableViewController {
     
     // MARK: Outlets
     
+    @IBOutlet weak var barLabel: UILabel!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var emoji1: UIButton!
+    
+    @IBOutlet weak var emoji2: UIButton!
+    
+    
+    @IBOutlet weak var emoji3: UIButton!
+    
+    @IBOutlet weak var emoji4: UIButton!
+    
+    @IBOutlet weak var emoji5: UIButton!
+    
+    @IBOutlet weak var emoji6: UIButton!
+    
+    @IBOutlet weak var emoji7: UIButton!
+    
+    @IBOutlet weak var emoji8: UIButton!
+    
+    
+    @IBOutlet weak var emoji9: UIButton!
+    
+    @IBOutlet weak var emoji10: UIButton!
+    
+    @IBOutlet weak var emoji11: UIButton!
+    
+    @IBOutlet weak var emoji12: UIButton!
+    
+    @IBOutlet weak var emoji13: UIButton!
+    
+    
+    @IBOutlet weak var emoji14: UIButton!
+    
+    
+    @IBOutlet weak var emoji15: UIButton!
+    
+    @IBOutlet weak var emoji16: UIButton!
+    
     
     // MARK: Actions
     
+    
+    @IBAction func reselectBarButtonTapped(sender: UIButton) {
+    }
+    
+    @IBAction func emoji1(sender: UIButton) {
+    }
+    
+    @IBAction func emoji2(sender: UIButton) {
+    }
+    
+    
+    @IBAction func emoji3(sender: UIButton) {
+    }
+    
+    
+    @IBAction func emoji4(sender: UIButton) {
+    }
+    
+    @IBAction func emoji5(sender: UIButton) {
+    }
+    
+    @IBAction func emoji6(sender: UIButton) {
+    }
+    
+    @IBAction func emoji7(sender: UIButton) {
+    }
+    
+    @IBAction func emoji8(sender: UIButton) {
+    }
+    
+    @IBAction func emoji9(sender: UIButton) {
+    }
+    
+    
+    @IBAction func emoji10(sender: UIButton) {
+    }
+    
+    @IBAction func emoji11(sender: UIButton) {
+    }
+    
+    @IBAction func emoji12(sender: UIButton) {
+    }
+    
+    
+    @IBAction func emoji13(sender: UIButton) {
+    }
+    
+    @IBAction func emoji14(sender: UIButton) {
+    }
+    
+    
+    @IBAction func emoji15(sender: UIButton) {
+    }
+    
+    
+    @IBAction func emoji16(sender: UIButton) {
+    }
     
     // MARK: viewDid Functions
     
