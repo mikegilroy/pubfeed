4e82ce465d7f8ad1bce3aa29bc54ac2625fc7108
Edward Suczewski <ed.suczewski@gmail.com>
2016-01-11 14:51:27 -0700

    emoji switch functions on NewPostTableVC
    Signed-off-by: Edward Suczewski <ed.suczewski@gmail.com>

--- PubFeed/NewPostTableTableViewController.swift
+++ PubFeed/NewPostTableTableViewController.swift
@@ -12,6 +12,8 @@ class NewPostTableTableViewController: UITableViewController {
 
     // MARK: Properties
     var selectedBar: Bar?
+    var selectedEmoji: String?
+    var emojiMenu = ["❤️", "💦", "💋", "😰", "😤", "🔥", "💣", "🍆", "🤗", "🍑", "🍼", "✊🏿", "😥", "🐣", "😘", "😈"]
     
     // MARK: Outlets
     
@@ -58,12 +60,69 @@ class NewPostTableTableViewController: UITableViewController {
     
     // MARK: Actions
     
-    
     @IBAction func reselectBarButtonTapped(sender: UIButton) {
     }
     
     @IBAction func emoji1(sender: UIButton) {
+        let alpha = CGFloat(0.5)
+        resetSelectedEmojiToDefaultAlpha(alpha)
+        switch sender.tag {
+        case 1:
+            emojiMenu.indexOf(selectedEmoji)
+            emoji1.alpha = CGFloat(1.0)
+            selectedEmoji = emojiMenu[0]
+        case 2:
+            emoji2.alpha = CGFloat(1.0)
+            selectedEmoji = emojiMenu[1]
+        case 3:
+            emoji3.alpha = CGFloat(1.0)
+            selectedEmoji = emojiMenu[2]
+        case 4:
+            emoji4.alpha = CGFloat(1.0)
+            selectedEmoji = emojiMenu[3]
+        case 5:
+            emoji5.alpha = CGFloat(1.0)
+            selectedEmoji = emojiMenu[4]
+        case 6:
+            emoji6.alpha = CGFloat(1.0)
+            selectedEmoji = emojiMenu[5]
+        case 7:
+            emoji7.alpha = CGFloat(1.0)
+            selectedEmoji = emojiMenu[6]
+        case 8:
+            emoji8.alpha = CGFloat(1.0)
+            selectedEmoji = emojiMenu[7]
+        case 9:
+            emoji9.alpha = CGFloat(1.0)
+            selectedEmoji = emojiMenu[8]
+        case 10:
+            emoji10.alpha = CGFloat(1.0)
+            selectedEmoji = emojiMenu[9]
+        case 11:
+            emoji11.alpha = CGFloat(1.0)
+            selectedEmoji = emojiMenu[10]
+        case 12:
+            emoji12.alpha = CGFloat(1.0)
+            selectedEmoji = emojiMenu[11]
+        case 13:
+            emoji13.alpha = CGFloat(1.0)
+            selectedEmoji = emojiMenu[12]
+        case 14:
+            emoji14.alpha = CGFloat(1.0)
+            selectedEmoji = emojiMenu[13]
+        case 15:
+            emoji15.alpha = CGFloat(1.0)
+            selectedEmoji = emojiMenu[14]
+        case 16:
+            emoji16.alpha = CGFloat(1.0)
+            selectedEmoji = emojiMenu[15]
+        }
     }
+    
+    
+    
+    
+    
     
     @IBAction func emoji2(sender: UIButton) {
     }
@@ -120,8 +179,49 @@ class NewPostTableTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
     }
+    
+    // MARK: Functions
+    func resetSelectedEmojiToDefaultAlpha(alpha: CGFloat) {
+        
+        switch emojiMenu.indexOf(selectedEmoji) {
+        case 0:
+            emoji1.alpha = alpha
+        case 1:
+            emoji2.alpha = alpha
+        case 2:
+            emoji3.alpha = alpha
+        case 3:
+            emoji4.alpha = alpha
+        case 4:
+            emoji5.alpha = alpha
+        case 5:
+            emoji6.alpha = alpha
+        case 6:
+            emoji7.alpha = alpha
+        case 7:
+            emoji8.alpha = alpha
+        case 8:
+            emoji9.alpha = alpha
+        case 9:
+            emoji10.alpha = alpha
+        case 10:
+            emoji11.alpha = alpha
+        case 11:
+            emoji12.alpha = alpha
+        case 12:
+            emoji13.alpha = alpha
+        case 13:
+            emoji14.alpha = alpha
+        case 14:
+            emoji15.alpha = alpha
+        case 15:
+            emoji16.alpha = alpha
+        default:
+            print("emoji selection error")
+        }
+    }
+        
 
     
 }
