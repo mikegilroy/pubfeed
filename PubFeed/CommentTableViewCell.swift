87796f2ecea994ac6844f0d00b3d584e940ea8de
Thang Tong <tongthang0104@gmail.com>
2016-01-11 17:34:05 -0700

    PostDetailTableView

--- PubFeed/CommentTableViewCell.swift
+++ PubFeed/CommentTableViewCell.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class CommentTableViewCell: UITableViewCell {
+    
+    var user: User?
 
+    @IBOutlet weak var profilePhoto: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func updateWithComment(comment: Comment) {
+        
+//        ImageController.profilePhotoForIdentifier((UserController.sharedController.currentUser?.photo)!, user: UserController.sharedController.currentUser!) { (photoUrl) -> Void in
+//            self.profilePhoto.image = photoUrl
+//        
+//        }
+        self.profilePhoto.image = UIImage(named: (UserController.sharedController.currentUser?.photo)!)
+        self.usernameLabel.text = UserController.sharedController.currentUser?.username
+        self.commentLabel.text = comment.text
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
--- PubFeed/PostDetailViewController.swift
+++ PubFeed/PostDetailViewController.swift
@@ -11,14 +11,21 @@ import UIKit
 class PostDetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     // MARK: Properties
-    
+    var user: User?
+    var comment: Comment?
     
     // MARK: Outlets
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var profilePhoto: UIImageView!
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var postText: UILabel!
     
     
     // MARK: Actions
+    
+    
+    
     
     
     // MARK: viewDid Functions
@@ -36,6 +43,14 @@ class PostDetailViewController: UIViewController, UITableViewDataSource, UITable
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("commentCell", forIndexPath: indexPath)
+//        ImageController.profilePhotoForIdentifier((UserController.sharedController.currentUser?.photo)!, user: UserController.sharedController.currentUser!) { (photoUrl) -> Void in
+////            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+////                 cell.imageView?.image = photoUrl
+//////            })
+////            tableView.reloadData()
+//            
+//        }
+  
         return cell
     }
 
