21e0897dd2e534f04dc38e50c5fcfebf70f00890
Mike Gilroy <mjgilroy@me.com>
2016-01-17 14:02:22 -0700

    added green green tint color extension

--- PubFeed/UIColorExtension.swift
+++ PubFeed/UIColorExtension.swift
@@ -13,4 +13,8 @@ extension UIColor {
     func pinkTintColor() -> UIColor {
         return UIColor(red: 234/255, green: 49/255, blue: 84/255, alpha: 1.0)
     }
+    
+    func greenTintColor() -> UIColor {
+        return UIColor(red: 106/255, green: 255/255, blue: 99/255, alpha: 1.0)
+    }
 }
