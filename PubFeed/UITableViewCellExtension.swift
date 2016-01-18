ad99399b3335dbb5a8a97ae2415f64b158cfb158
Edward Suczewski <ed.suczewski@gmail.com>
2016-01-17 19:55:57 -0700

    actually fixed it this time
    Signed-off-by: Edward Suczewski <ed.suczewski@gmail.com>

--- PubFeed/UITableViewCellExtension.swift
+++ PubFeed/UITableViewCellExtension.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+extension UITableViewCell {
+    
+    func addCustomSeperator(lineColor: UIColor) {
+        let seperatorView = UIView(frame: CGRect(x: 0, y: self.frame.height - 1, width: self.frame.width, height: 1))
+        seperatorView.backgroundColor = lineColor
+        self.addSubview(seperatorView)
+    }
+    
+}
