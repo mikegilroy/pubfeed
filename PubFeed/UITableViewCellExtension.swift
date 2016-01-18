7816a06bd5767868cce985555266ebecd3aa4925
Edward Suczewski <ed.suczewski@gmail.com>
2016-01-17 19:49:50 -0700

    fixed problem w UITableViewCellExtension
    Signed-off-by: Edward Suczewski <ed.suczewski@gmail.com>

--- PubFeed/UITableViewCellExtension.swift
+++ PubFeed/UITableViewCellExtension.swift
@@ -2,18 +2,8 @@
 //  UITableViewCellExtension.swift
 //  PubFeed
 //
-//  Created by Mike Gilroy on 17/01/2016.
+//  Created by Edward Suczewski on 1/17/16.
 //  Copyright © 2016 Mike Gilroy. All rights reserved.
 //
 
 import Foundation
-
-extension UITableViewCell {
-    
-    func addCustomSeperator(lineColor: UIColor) {
-        let seperatorView = UIView(frame: CGRect(x: 0, y: self.frame.height - 1, width: self.frame.width, height: 1))
-        seperatorView.backgroundColor = lineColor
-        self.addSubview(seperatorView)
-    }
-    
-}
