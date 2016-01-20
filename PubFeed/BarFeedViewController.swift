1d7192cd360f99c20109231b25a5f71810156417
Thang Tong <tongthang0104@gmail.com>
2016-01-20 14:09:12 -0700

    updated

--- PubFeed/BarFeedViewController.swift
+++ PubFeed/BarFeedViewController.swift
@@ -122,10 +122,8 @@ class BarFeedViewController: UIViewController, UITableViewDataSource, UITableVie
         super.viewDidLoad()
         configureTableView()
         loadBarDetails()
-        loadPostsForBar()
-        dispatch_async(dispatch_get_main_queue(), { () -> Void in
-            self.tableView.reloadData()
-        })
+//        loadPostsForBar()
+     
         
         if let bar = bar {
             bar.setAsCurrent()
@@ -243,17 +241,28 @@ class BarFeedViewController: UIViewController, UITableViewDataSource, UITableVie
                                 self.posts?.removeAtIndex(indexOfPost)
                                 posts = self.posts!
                               
+                            } else {
+                                posts = self.posts!
+                                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                                    self.tableView.reloadData()
+                                })
                             }
                         } else {
                             print("no post")
                             posts = self.posts!
+                            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                                self.tableView.reloadData()
+                            })
                           
                         }
-                        
+//                        
                         dispatch_async(dispatch_get_main_queue(), { () -> Void in
                             self.tableView.reloadData()
                         })
                     })
+//                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                        self.tableView.reloadData()
+//                    })
                 }
             }
         }
